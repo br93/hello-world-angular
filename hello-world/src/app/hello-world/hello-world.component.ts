1fee3083f1ac630ad3217545ce0ee5bebb442dde
br93 <reisalvesb@gmail.com>
2024-06-28 15:48:29 -0300

    first commit

---
+++ hello-world/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'hello-world',
+    template: `
+    <p>Hello World from component</p>
+    `
+})
+export class HelloWorldComponent { }
